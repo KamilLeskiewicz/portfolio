fbdda60c1856ca8008a78cb12e0ec725ed83cee7
SmoczaSkala <kamilo.dance2000@gmail.com>
2025-05-19 02:03:03 +0200

    somechanges

--- components/projects.tsx
+++ components/projects.tsx
@@ -32,7 +32,7 @@ export default function Projects() {
     {
       title: "Weather App",
       description: "A simple mobile weather app that shows detailed information about the location entered by the user.",
-      image: "/wheather.jpg",
+      image: "/weather.jpg",
       tags: ["ReactNative", "TypeScript", "JavaScript", "swift", "xCode", "expo"],
       liveUrl: "#",
       githubUrl: "https://github.com/KamilLeskiewicz/Pogodynka",
