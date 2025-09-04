74d63024ccb8791db3c19065bd2027528d3bd0fd
SmoczaSkala <kamilo.dance2000@gmail.com>
2025-09-04 13:56:04 +0200

    added tracker

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,12 @@ import Projects from "@/components/projects";
 import Contact from "@/components/contact";
 import ScrollToTop from "@/components/scroll-to-top";
 import Roadmap from '../components/Roadmap';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
       <link rel="icon" href="/Logo.png" sizes="any" />
+      <SpeedInsights />
       <Hero />
       <About />
       <Roadmap />
